af8aea4a007d9cf28250fc6dfdb686a2323fa8bd
Txomin <txominvs@gmail.com>
2026-06-14 17:43:17 +0200

    Add vector database insertion and retrival examples and allow Ollama to unload LLMs from memory after a minute

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 import ollama
+import chromadb
 
-client = ollama.Client(host="http://localhost:11434")
+ollama_client = ollama.Client(host="http://localhost:11434")
 
-llm_response = client.chat(
+chroma_client = chromadb.HttpClient(host="localhost", port=8000)
+chroma_collection = chroma_client.get_or_create_collection("rag_context")
+
+# Sample LLM query
+llm_response = ollama_client.chat(
     model="qwen2.5:0.5b",
     messages=
     [
@@ -15,9 +20,27 @@ llm_response = client.chat(
 
 print(llm_response)
 
-embedding_response = client.embeddings(
+# Sample embedding generation
+original_text = "Some sample text"
+
+embedding_response = ollama_client.embeddings(
     model="nomic-embed-text",
-    prompt="Some sample text"
+    prompt=original_text
 ).embedding
 
 print(embedding_response)
+
+# Sample insertion into DB
+chroma_collection.add(
+    ids="0",
+    embeddings=embedding_response,
+    documents=original_text
+)
+
+# Sample retrival from DB
+documents = chroma_collection.query(
+    query_embeddings=embedding_response,
+    n_results=1
+)["documents"]
+
+print(documents)
